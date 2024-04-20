c9419858762b76970db5b629f65fc83656b09dcc
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-04-20 17:45:25 +0530

    Create SearchInRotatedSortedArray.py

---
+++ binarySearch/SearchInRotatedSortedArray.py
@@ -0,0 +1,35 @@
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low=0
+        high=n-1
+
+        while low<=high:
+
+            mid=(low+high)//2
+
+            if nums[mid]==target:
+                return True
+            
+            if(nums[low]==nums[mid] and nums[high]==nums[mid]):
+                low+=1
+                high-=1
+                continue
+
+        
+            #check left array is sorted or not
+            if nums[low]<=nums[mid]:
+                if(nums[low]<=target and target<=nums[mid]):
+                    high=mid-1 
+                else:
+                    low=mid+1
+            
+            else:
+                if(nums[mid]<=target and target<=nums[high]):
+                    low=mid+1
+                else:
+                    high=mid-1
+
+        return False
+        
