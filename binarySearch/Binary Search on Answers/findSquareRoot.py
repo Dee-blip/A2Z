25f627e52d7e4c2f71c71ca4df9998e9830e3964
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-04-21 23:38:47 +0530

    Create findSquareRoot.py

---
+++ binarySearch/Binary Search on Answers/findSquareRoot.py
@@ -0,0 +1,24 @@
+#https://www.naukri.com/code360/problems/square-root-integral_893351?leftPanelTab=0%3Futm_source%3Dstriver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+import math
+def floorSqrt(n):
+   # Simple Approach - O(1)
+   # return int(math.sqrt(n))
+   #i*i<=n then its our answer
+
+   # binary search
+   low=1
+   high=n
+   ans=n
+
+   while low<=high:
+      mid=(low+high)//2
+
+      if mid*mid>n:
+         high=mid-1
+      else:
+         ans=mid 
+         low=mid+1
+   return ans
+   
+n= int(input())
+print(floorSqrt(n))
