ff55e41c7ed3e074291b0d87388eb7c9486ee105
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-05-25 17:27:56 +0530

    Update preOrderTraversal.py

--- binaryTree/preOrderTraversal.py
+++ binaryTree/preOrderTraversal.py
@@ -10,3 +10,29 @@ class Solution:
             return []
         return [root.val] +self.preorderTraversal(root.left) +self.preorderTraversal(root.right) 
         
+
+# Iterative
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root==None or root==[]:
+            return []
+        ans=[]
+        q=[root]  
+        while len(q):
+            temp=q.pop()
+            ans.append(temp.val)
+             
+            if temp.right is not None:
+                q.append(temp.right)
+            if temp.left is not None:
+                q.append(temp.left)
+
+        return ans
+
+             
