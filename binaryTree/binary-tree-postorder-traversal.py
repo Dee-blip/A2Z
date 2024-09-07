168edad2d4805c55293bec15b985603403353f54
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-09-07 17:36:57 +0530

    Update binary-tree-postorder-traversal.py

--- binaryTree/binary-tree-postorder-traversal.py
+++ binaryTree/binary-tree-postorder-traversal.py
@@ -6,28 +6,51 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root==None:
-            return []
-
-        stack1=[]
-        stack2=[]
-
+        #using one stack
+        curr=root
+        stack=[]
         res=[]
-        #post order-  Left Right Root
-        stack1.append(root)
-        while(len(stack1)!=0):
-            temp= stack1[-1]
-            stack1.pop()
-            stack2.append(temp)
-
-            if temp.left is not None:
-                stack1.append(temp.left)
-            if temp.right is not None:
-                stack1.append(temp.right)
-    
-        for i in range(len(stack2)-1,-1,-1):
-            res.append(stack2[i].val)
-
+        while curr is not None or len(stack)!=0 :
+            if curr!=None:
+                stack.append(curr)
+                curr=curr.left
+            else:
+                temp = stack[-1].right
+                if(temp==None):
+                    temp=stack[-1]
+                    stack.pop()
+                    res.append(temp.val)
+                    while len(stack)!=0 and temp==stack[-1].right:
+                        temp=stack[-1]
+                        stack.pop()
+                        res.append(temp.val)
+                else:
+                    curr=temp
         return res
+
+        # using 2 stack
+        # if root==None:
+        #     return []
+
+        # stack1=[]
+        # stack2=[]
+
+        # res=[]
+        # #post order-  Left Right Root
+        # stack1.append(root)
+        # while(len(stack1)!=0):
+        #     temp= stack1[-1]
+        #     stack1.pop()
+        #     stack2.append(temp)
+
+        #     if temp.left is not None:
+        #         stack1.append(temp.left)
+        #     if temp.right is not None:
+        #         stack1.append(temp.right)
+    
+        # for i in range(len(stack2)-1,-1,-1):
+        #     res.append(stack2[i].val)
+
+        # return res
 
 
