aab7fac7583b98e74576063d844e0d2063d2f7c7
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-09-29 01:29:32 +0530

    Update stock_span_problem.py

--- Stack&Queues/stock_span_problem.py
+++ Stack&Queues/stock_span_problem.py
@@ -17,3 +17,19 @@ class Solution:
                 span[i]=i-stack[-1]
             stack.append(i)
         return span
+
+class StockSpanner:
+    def __init__(self):
+   
+        self.st = []
+
+    def next(self, price: int) -> int:
+        span = 1  
+        
+        while self.st and self.st[-1][0] <= price:
+            span += self.st.pop()[1] 
+        
+        self.st.append((price, span))
+        
+        return span
+        
