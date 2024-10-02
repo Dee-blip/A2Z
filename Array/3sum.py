838be4e8a627d48a135ea687d6fe0e6f97fc67b5
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-02 23:27:15 +0530

    Update 3sum.py

--- Array/3sum.py
+++ Array/3sum.py
@@ -2,27 +2,22 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n=len(nums)
         nums.sort()
-        res=[]
+        res=set()
 
         for i in range(n-2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
             j=i+1
             k=n-1
-
             while j<k:
                 temp = nums[i]+nums[j]+nums[k]
-
                 if temp==0:
-                    if ([nums[i],nums[j],nums[k]]) not in res:
-                        res.append([nums[i],nums[j],nums[k]])
+                    res.add((nums[i], nums[j], nums[k])) 
                     j+=1
                     k-=1
                 elif temp>0:
                     k-=1
                 else:
                     j+=1
-        return res
+        return [list(i) for i in res]
 
 
 
