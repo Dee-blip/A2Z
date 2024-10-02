e3f161920e4745a7ff227e056fea74d445bd7e53
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-02 14:02:35 +0530

    Create jump-game.py

---
+++ Greedy Algorithms/jump-game.py
@@ -0,0 +1,14 @@
+#-https://leetcode.com/problems/jump-game/
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxIndex=0
+        n=len(nums)
+        for i in range(n):
+            if i>maxIndex:
+                return False
+            maxIndex=max(maxIndex,i+nums[i])
+
+            if maxIndex>=n-1:
+                return True
+        return False
+        
