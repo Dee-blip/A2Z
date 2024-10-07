7e0f36b36da679fa8c4415659e4cebf783f1577f
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-08 00:06:25 +0530

    Create climbing_stairs.py

---
+++ DP/climbing_stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp=[-1 for i in range(n+1)]
+        return self.helper(n,dp) 
+    # 1-step = from n-1 to n
+    # 2-step = from n-2 to n
+    def helper(self,n,dp):
+            if n<=1:
+                return 1
+            if dp[n]!=-1:
+                return dp[n]
+            dp[n]= self.helper(n-1,dp) + self.helper(n-2,dp)
+            return dp[n]
+
+        
