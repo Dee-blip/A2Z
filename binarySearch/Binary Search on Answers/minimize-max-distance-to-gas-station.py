c817aa79ea9533b16c001e96aeaabc959f116149
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-15 23:53:32 +0530

    Create minimize-max-distance-to-gas-station.py

---
+++ binarySearch/Binary Search on Answers/minimize-max-distance-to-gas-station.py
@@ -0,0 +1,35 @@
+#https://www.geeksforgeeks.org/problems/minimize-max-distance-to-gas-station/1
+
+class Solution:
+    
+    def check(self,stations, k, mid):
+        count = 0
+        n = len(stations)
+        
+        for i in range(1, n):
+            diff = stations[i] - stations[i - 1]
+            if diff > mid:
+                count += diff // mid
+        
+        if count > k:
+            return False
+        else:
+            return True
+
+    def findSmallestMaxDist(self,stations, k):
+        lo = 0
+        n = len(stations)
+        hi = stations[n - 1] - stations[0]
+        
+        ans = 0
+        
+        while lo < hi:
+            mid = (lo + hi) / 2
+            
+            if self.check(stations, k, mid):
+                ans = mid
+                hi = mid - 0.000001
+            else:
+                lo = mid + 0.000001
+        
+        return ans
