e234266d2dbe082cdb64acc422a02cd4d6d5bfbb
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-17 00:18:31 +0530

    Update gas_station.py

--- Greedy Algorithms/gas_station.py
+++ Greedy Algorithms/gas_station.py
@@ -1,23 +1,22 @@
 #https://leetcode.com/problems/gas-station/
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        total_gas=0
+
         current_gas=0
-        total_cost= 0
         start_index=0
 
         for i in range(len(gas)):
-            total_gas+=gas[i]
-            total_cost+=cost[i]
+            # total_gas+=gas[i]
+            # total_cost+=cost[i]
 
             current_gas+=gas[i]-cost[i]
 
             if current_gas <0:
                 start_index=i+1
                 current_gas=0
-
-        if total_gas<total_cost:
+        # what we have < what we need then we have to return -1 else print start_index
+        if sum(gas)<sum(cost):
             return -1
-        return start_index
+        return start_inde
 
         
