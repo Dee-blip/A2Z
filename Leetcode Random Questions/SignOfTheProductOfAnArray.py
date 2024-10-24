da5734d582af9c2eabf510a95c258384de719c41
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2024-10-24 22:43:54 +0530

    Create SignOfTheProductOfAnArray.py

---
+++ Leetcode Random Questions/SignOfTheProductOfAnArray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        res=1
+        for i in nums:
+            res*=i
+
+        if res==0:
+            return 0
+        elif res<0:
+            return -1
+        else:
+            return 1
+
+        
+        
